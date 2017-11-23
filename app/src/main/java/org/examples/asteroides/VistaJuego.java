47baaef96e9d89f0a82f0ddbb99742d8ff79b048
clara_rodriguez <clarirodri_83@hotmail.com>
2017-11-23 18:52:36 +0100

    Vectorial

--- app/src/main/java/org/examples/asteroides/VistaJuego.java
+++ app/src/main/java/org/examples/asteroides/VistaJuego.java
@@ -1,8 +1,15 @@
 package org.examples.asteroides;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.PathShape;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -27,8 +34,49 @@ public class VistaJuego extends View {
     public VistaJuego(Context context, AttributeSet attrs){
         super(context, attrs);
         Drawable drawableNave, drawableAsteroide, drawableMisil;
-        drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
-        drawableNave = context.getResources().getDrawable(R.drawable.nave);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        if(pref.getString("graficos", "1").equals("0")){
+            Path pathAsteroide = new Path();
+            pathAsteroide.moveTo((float) 0.3, (float) 0.0);
+            pathAsteroide.lineTo((float) 0.6, (float) 0.0);
+            pathAsteroide.lineTo((float) 0.6, (float) 0.3);
+            pathAsteroide.lineTo((float) 0.8, (float) 0.2);
+            pathAsteroide.lineTo((float) 1.0, (float) 0.4);
+            pathAsteroide.lineTo((float) 0.8, (float) 0.6);
+            pathAsteroide.lineTo((float) 0.9, (float) 0.9);
+            pathAsteroide.lineTo((float) 0.8, (float) 1.0);
+            pathAsteroide.lineTo((float) 0.4, (float) 1.0);
+            pathAsteroide.lineTo((float) 0.0, (float) 0.6);
+            pathAsteroide.lineTo((float) 0.0, (float) 0.2);
+            pathAsteroide.lineTo((float) 0.3, (float) 0.0);
+            ShapeDrawable dAsteroide = new ShapeDrawable(new PathShape(pathAsteroide, 1, 1));
+            dAsteroide.getPaint().setColor(Color.WHITE);
+            dAsteroide.getPaint().setStyle(Paint.Style.STROKE);
+            dAsteroide.setIntrinsicWidth(50);
+            dAsteroide.setIntrinsicHeight(50);
+            drawableAsteroide = dAsteroide;
+            setBackgroundColor(Color.BLACK);
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            //nave
+            Path pathNave = new Path();
+            pathNave.moveTo((float) 0, (float) 0);
+            pathNave.lineTo((float) 1.0, (float) 0.5);
+            pathNave.lineTo((float) 0.0, (float) 1.0);
+            pathNave.lineTo((float) 0.0, (float) 0.0);
+            ShapeDrawable dNave = new ShapeDrawable(new PathShape(pathNave, 1, 1));
+            dNave.getPaint().setColor(Color.WHITE);
+            dNave.getPaint().setStyle(Paint.Style.STROKE);
+            dNave.setIntrinsicHeight(15);
+            dNave.setIntrinsicWidth(20);
+            drawableNave = dNave;
+        } else{
+            drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+            setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            //nave
+            drawableNave = context.getResources().getDrawable(R.drawable.nave);
+        }
+
         nave = new Grafico(this, drawableNave);
         asteroides = new Vector<Grafico>();
         for (int i = 0; i < numAsteroides; i++){
